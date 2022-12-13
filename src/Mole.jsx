18cbeaa59954b4a318cbb4b25918c0b5b1f32f8f
danazou <dana.sou@gmail.com>
2022-12-12 19:43:53 -0800

    implementing useEffect and function for timer

--- src/Mole.jsx
+++ src/Mole.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import {useEffect} from 'react'
+import moleImg from './images/mole.png'
 
-function Mole() {
+function Mole(props){
+    useEffect(() => {
+        // Feel free to adjust this number as you see fit.
+        // It is your game to tune!
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
     return (
-      
-    );
-  }
-  
-
-  export default Mole
+        <div>
+            <img style={{'width': '30vw'}}
+            src={moleImg}
+            onClick={props.handleClick} />
+        </div>
+    )
+}
